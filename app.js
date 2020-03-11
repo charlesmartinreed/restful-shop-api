a28c632f47fcb17f0626f3adff56651fff405f86
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-10 19:38:42 -0500

    Add error handling middleware in app.js.

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 // MIDDLEWARE - incoming requests pass through here
-// app.use((req, res, next) => {
-//   res.status(200).json({
-//     message: "Working just fine."
-//   });
-// });
+
+app.use(logger("dev")); // running morgan logger in "dev" mode - logs in the console
+
+// SUPPORTED ROUTES
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+// NON-SUPPORTED ROUTES
+app.use((req, res, next) => {
+  // error obj derived from node.js
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// error handler; from failed routes, db operations, etc
+app.use((error, req, res, next) => {
+  // thrown errors caught here
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
