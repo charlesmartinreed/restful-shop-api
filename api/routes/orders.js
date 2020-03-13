2eb8afff148645ed6ca3001f937aa00b487f61df
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-13 18:07:19 -0500

    Create order schema. Implement GET all and POST req for orders route.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("../models/orders");
 
 // ORDERS - GET all
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "GET request for orders route received"
-  });
+  Order.find()
+    .select("product quantity _id")
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc._productId,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: `http://localhost:${process.env.PORT}/orders/${doc._id}`
+            }
+          };
+        })
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error });
+    });
 });
 
 // ORDERS - GET by id
@@ -19,10 +42,31 @@ router.get("/:orderId", (req, res, next) => {
 // ORDERS - POST new order
 router.post("/", (req, res, next) => {
   const { productId, quantity } = req.body;
-  res.status(201).json({
-    message: "New order was created!",
-    order: { productId, quantity }
+
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity,
+    product: productId
   });
+
+  order
+    .save()
+    .then(result => {
+      // contains order id AND product id
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error
+      });
+    });
+
+  // res.status(201).json({
+  //   message: "New order was created!",
+  //   order: { productId, quantity }
+  // });
 });
 
 // DELETE - remove order by id
