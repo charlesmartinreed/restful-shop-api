70a564e944cd9704c4d751684cb18d4dcd4c69ea
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-14 16:35:17 -0500

    Add check for prexisting user. Add Delete user route. Add email validation regex.

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
+// unique adds some performance optimizations for storing, retreiving the email
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   },
   password: {
     type: String,
--- api/routes/user.js
+++ api/routes/user.js
@@ -5,38 +5,62 @@ const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
-// CREATE A NEW USER
+// POST - CREATE USER
 router.post("/signup", (req, res, next) => {
-  // hashes are one way, can't be 'unhashed'
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        error: err
-      });
+  // check for existence of current email in DB
+  User.find({ email: req.body.email }).then(user => {
+    // no user returns an empty array, not null
+    if (user.length >= 1) {
+      // 409 Conflict - could not process request because of conflift in current state of resource
+      return res
+        .status(409)
+        .json({ message: "User already exists with this email address" });
     } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash
-      });
-
-      //   save the newly created user to database
-      user
-        .save()
-        .then(result => {
-          console.log(result);
-          res.status(201).json({
-            message: "New user created"
+      // IF NO CURRENT USER, CREATE NEW ONE
+      // hashes are one way, can't be 'unhashed'
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err
           });
-        })
-        .catch(error => {
-          console.log(error);
-          res.status(500).json({ error });
-        });
+        } else {
+          const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: req.body.email,
+            password: hash
+          });
+
+          //   save the newly created user to database
+          user
+            .save()
+            .then(result => {
+              console.log(result);
+              res.status(201).json({
+                message: "New user created"
+              });
+            })
+            .catch(error => {
+              console.log(error);
+              res.status(500).json({ error });
+            });
+        }
+      });
     }
   });
 });
 
-// LOGIN A CURRENT USER
+// DELETE - REMOVE AN EXISTING USER
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .then(result => {
+      res.status(200).json({
+        message: "User was successfully removed"
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error });
+    });
+});
 
 module.exports = router;
