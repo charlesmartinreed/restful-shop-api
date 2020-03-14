3ff12c3becef785ae19351770b19dcae11ee80ef
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-14 17:19:09 -0500

    Add auth testing/token generation in user route.

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-// POST - CREATE USER
+// POST -- CREATE USER
 router.post("/signup", (req, res, next) => {
   // check for existence of current email in DB
   User.find({ email: req.body.email }).then(user => {
@@ -49,7 +50,55 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
-// DELETE - REMOVE AN EXISTING USER
+// POST -- LOGIN EXISTING USER
+router.post("/login", (req, res, next) => {
+  //  check for existence of user
+  User.find({ email: req.body.email })
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Authentication failed"
+        });
+      }
+
+      // if user found, check param passed password against stored password
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authentication failed"
+          });
+        }
+        if (result) {
+          // if passwords match, create a JSON Token
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1hr" }
+          );
+
+          return res.status(200).json({
+            message: "Authentication successful",
+            token
+          });
+        }
+
+        res.status(401).json({
+          message: "Authentication failed"
+        });
+      });
+    })
+    .catch(error => {
+      console.log(err);
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+// DELETE -- REMOVE AN EXISTING USER
 router.delete("/:userId", (req, res, next) => {
   User.remove({ _id: req.params.userId })
     .then(result => {
